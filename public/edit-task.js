c5c582940c776f048f2698530b68f1c5a51d3af8
Moleso587587 <71471833+Moleso587587@users.noreply.github.com>
2021-12-14 13:30:40 -0700

    fixed naming error, made checking value work, and replaced stylesheet

--- public/edit-task.js
+++ public/edit-task.js
@@ -2,7 +2,7 @@ const TASKIDDOM = document.querySelector(".task-edit-id")
 const TASKNAMEDOM = document.querySelector(".task-edit-name")
 const TASKCOMPLETEDOM = document.querySelector(".task-edit-completed")
 const EDITFORMDOM = document.querySelector(".single-task-form")
-const EDITBTNDOM = document.querySelector(".single-edit-btn")
+const EDITBTNDOM = document.querySelector(".task-edit-btn")
 const FORMALERTDOM = document.querySelector(".form-alert")
 const PARAMS = window.location.search
 const ID = new URLSearchParams(PARAMS).get('id')
@@ -14,6 +14,7 @@ const SHOWTASK = async () => {
         const { _id: taskId, completed, name } = task
         TASKIDDOM.textContent = taskId
         TASKNAMEDOM.value = name
+        TASKCOMPLETEDOM.checked = completed;
         tempName = name
         if (completed) {
             TASKCOMPLETEDOM.checked = true
@@ -31,12 +32,7 @@ EDITFORMDOM.addEventListener('submit', async (e) => {
     try {
         const TASKNAME = TASKNAMEDOM.value
         const TASKCOMPLETED = TASKCOMPLETEDOM.checked
-        await axios.patch(`/api/v1/tasks/${ID}`, {
-            name: 'sdf'
-        })
-        console.log('dsf')
-
-        // const { _id: TASKID, completed, name } = TASK
+        await axios.patch(`/api/v1/tasks/${ID}`, { name: TASKNAME, completed: TASKCOMPLETED })
         FORMALERTDOM.style.display = 'block'
         FORMALERTDOM.textContent = 'Success, edited task'
         FORMALERTDOM.classList.add('text-success')
